8efa63cf0a2953a656bc5ea55bbdd0bf79d84c5f
Daniel Smith <DSmith19@sheffield.ac.uk>
2019-06-12 22:27:50 +0100

    Combined with original data frame for 50 complete data sets with metadata using map()

--- scripts/Final/imp_final.R
+++ scripts/Final/imp_final.R
@@ -1,4 +1,5 @@
 ### Imputation
+library(tidyverse)
 library(mice)
 
 source("scripts/Final/clean_final.R")
@@ -72,4 +73,55 @@ e_list <- split(e_complete, f = e_complete$.imp)
 b_complete <- mice::complete(b_imputed, 'long')
 b_list <- split(b_complete, f = b_complete$.imp)
 
+# colnames wanted ---------------------------------------------------------
 
+df_nameswant <- c("species_id_stage_id",
+                  "body_size",
+                  "abundance",
+                  "node_type",
+                  "working_name",
+                  "organismal_group",
+                  "consumer_strategy_stage")
+
+
+
+# map() ---------------------------------------------------------------
+
+### csm
+
+c_list_bound <- map(c_list, cbind, c_nodes_fill)
+
+c_list_mutate <- map(c_list_bound, mutate,
+                     # name the mutate variables
+                     body_size = exp(log_body_size), 
+                     abundance = exp(log_abundance))
+
+c_list_select <- map(c_list_mutate, select, df_nameswant)
+
+### epb
+
+e_list_bound <- map(e_list, cbind, e_nodes_fill)
+
+e_list_mutate <- map(e_list_bound, mutate,
+                     # name the mutate variables
+                     body_size = exp(log_body_size), 
+                     abundance = exp(log_abundance))
+
+e_list_select <- map(e_list_mutate, select, df_nameswant)
+
+### bsq
+
+b_list_bound <- map(b_list, cbind, b_nodes_fill)
+
+b_list_mutate <- map(b_list_bound, mutate,
+                     # name the mutate variables
+                     body_size = exp(log_body_size), 
+                     abundance = exp(log_abundance))
+
+b_list_select <- map(b_list_mutate, select, df_nameswant)
+
+# keep --------------------------------------------------------------------
+
+gdata::keep(c_list_select, e_list_select, b_list_select, # complete list of 50 data sets combined
+            c_links_wrk, e_links_wrk, b_links_wrk, # links needed for matrix
+            sure = TRUE)
